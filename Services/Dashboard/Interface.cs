fa7bd4da8acd656c165feac67de42c513dfc62b9
markentingh <entingh@gmail.com>
2015-07-23 01:47:09 -0700

    updated interface CSS for dashboard

--- Services/Dashboard/Interface.cs
+++ Services/Dashboard/Interface.cs
@@ -12,16 +12,27 @@ namespace Rennder.Services.Dashboard
         {
             if (R.isSessionLost() == true) { return lostInject(); }
             Inject response = new Inject();
-
-            //check security
-            if (R.User.Website(R.Page.websiteId).getWebsiteSecurityItem("dashboard/pages", 4) == false) { return response; }
-
+            
             //setup response
             response.element = ".winDashboardInterface > .content";
 
             //setup scaffolding variables
-            Scaffold scaffold = new Scaffold(R, "/app/dashboard/interface.html", "", new string[] { "website-title" });
+            Scaffold scaffold = new Scaffold(R, "/app/dashboard/interface.html", "", 
+                new string[] { "website-title", "apps-list", "menu-pages", "menu-photos",
+                "menu-analytics", "menu-users", "menu-apps", "menu-settings"});
             scaffold.Data["website-title"] = R.Page.websiteTitle;
+
+            //check security
+            //if (R.User.Website(R.Page.websiteId).getWebsiteSecurityItem("dashboard/pages", 0) == true) { }
+            scaffold.Data["menu-pages"] = "true";
+            scaffold.Data["menu-photos"] = "true";
+            scaffold.Data["menu-analytics"] = "true";
+            scaffold.Data["menu-users"] = "true";
+            scaffold.Data["menu-apps"] = "true";
+            scaffold.Data["menu-settings"] = "true";
+
+            //load list of apps into dashboard menu
+            scaffold.Data["apps-list"] = "";
 
             //finally, scaffold Rennder platform HTML
             response.html = scaffold.Render();
